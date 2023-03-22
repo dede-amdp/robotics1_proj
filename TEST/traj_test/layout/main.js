8860f86db46089e09070e1661deeae35bddfea6f
dede-amdp <73134218+dede-amdp@users.noreply.github.com>
2023-03-22 09:49:41 +0100

    prototyping for getting trajectory points

--- TEST/traj_test/layout/main.js
+++ TEST/traj_test/layout/main.js
@@ -1,1 +1,77 @@
 'use strict';
+
+const settings = {
+    'origin': { 'x': 0, 'y': 250 },
+    'rwidth': 0.25,
+    'rheight': 0.25
+};
+
+
+const incanvas = document.getElementById('input_canvas');
+const ctx = incanvas.getContext('2d');
+const send_btn = document.getElementById('send_data_btn');
+var points = [];
+
+incanvas.addEventListener('click', handle_input);
+send_btn.addEventListener('click', handle_data);
+draw_background();
+
+
+// === FUNCTION DEFINITIONS === //
+
+eel.expose(jslog);
+function jslog(msg) {
+    console.log(msg);
+}
+
+
+eel.expose(jsget_points);
+function jsget_points() {
+    var temp = [];
+    for (var p of points) {
+        temp.push(p['actual']);
+    }
+    points = []; //empty the array
+    console.log("SENT DATA:", temp);
+    canvas_update();
+    return temp;
+}
+
+function draw_background(color = '#EEEEEE') {
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, incanvas.clientWidth, incanvas.clientHeight);
+}
+
+function handle_input(e) {
+    var boundary = e.target.getBoundingClientRect();
+    //DEBUG: console.log(boundary);
+    var x = e.clientX - boundary.left;
+    var y = e.clientY - boundary.top;
+    //DEBUG: console.log(x, y);
+    var px = x - settings['origin']['x'];
+    var py = (incanvas.height - y) - settings['origin']['y'];
+    var rx = (px / incanvas.width) * settings['rwidth'];
+    var ry = (py / incanvas.height) * settings['rheight'];
+    //DEBUG: console.log(rx, ry);
+    // add points to the list
+    points.push({ 'actual': { 'x': rx, 'y': ry }, 'relative': { x, y } });
+    // update the canvas
+    canvas_update();
+}
+
+function canvas_update() {
+    draw_background();
+    for (var p of points) {
+        ctx.fillStyle = '#000000';
+        ctx.beginPath();
+        ctx.arc(p['relative']['x'], p['relative']['y'], 7, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+function handle_data() {
+    console.log("Handling data");
+    eel.pyget_data();
+}
---
+++ TEST/traj_test/trajpy.py
@@ -0,0 +1,61 @@
+from mat import *
+
+point_time = tuple[float, float]
+
+def time_row(t:float, deg: int, der:int) -> list[float]:
+    row = []
+    for i in range(0, deg+1):
+        match der:
+            case 0: row.append(t**i)
+            case 1: row.append(i*t**(i-1) if i-1 >= 0 else 0)
+            case 2: row.append(i*(i-1)*t**(i-2) if i-2 >= 0 else 0)
+    return row
+
+
+def spline3(q: list[point_time], dq: list[point_time])->mat: #, ddq: list[point_time])-> list:
+    '''
+    q = a0+a1t+a2t2+a3t3
+    dq = a1+2a2t+3a3t2
+    ddq = 2a2+6a3t
+    '''
+    vandermont = []
+    for point in q:
+        vandermont.append(time_row(point[1],3,0))
+    for point in dq:
+        vandermont.append(time_row(point[1],3,1))
+    '''for point in ddq:
+        vandermont.append(time_row(point[1],3,2))'''
+    known_terms = [[point[0]] for point in q+dq] #+ddq]
+    A = mat(vandermont)
+    b = mat(known_terms)
+    return A.inv().dot(b)
+
+def spline5(q: list[point_time], dq: list[point_time], ddq: list[point_time])->mat:
+    '''
+    q = a0+a1t+a2t2+a3t3+a4t4+a5t5
+    dq = a1+2a2t+3a3t2+4a4t3+5a5t4
+    ddq = 2a2+6a3t+12a4t2+20a5t3
+    '''
+    vandermont = []
+    for point in q:
+        vandermont.append(time_row(point[1],5,0))
+    for point in dq:
+        vandermont.append(time_row(point[1],5,1))
+    for point in ddq:
+        vandermont.append(time_row(point[1],5,2))
+    known_terms = [[point[0]] for point in q+dq+ddq]
+    A = mat(vandermont)
+    b = mat(known_terms)
+    return A.inv().dot(b)
+
+def rangef(start:float=0, step:float=1, end:float=0) -> list:
+    if step == 0: return []
+    if start >= end: return []
+    if start < end and step < 0: return []
+    if start > end and step > 0: return []
+    r = []
+    i = start
+    while i < end:
+        r.append(i)
+        i += step
+    return r
